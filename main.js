10307812661b1970c0a1028310fdef80011f397f
DoTMInDs <123625827+DoTMInDs@users.noreply.github.com>
2023-03-14 10:18:46 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+/*function addEvent(){
+
+    var left = document.getElementById("left").style.display = "block"
+}
+
+*/
+
+// let menu = document.querySelector('.menu-icon');
+// let leftnav = document.querySelector('.left-nav');
+
+// menu.onclick = function() {
+//     leftnav.classList.toggle('resize-left');
+// }
