5b31b6436fd4d9e2bce217af8f25dc0b1a0c421d
D•M•InDz <123625827+DoTMInDs@users.noreply.github.com>
2023-10-06 12:33:29 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,46 @@
-/*function addEvent(){
 
-    var left = document.getElementById("left").style.display = "block"
+var navbar = document.getElementById("navbar");
+
+function openmenu(){
+    navbar.style.right = '0';
+    
+}
+function closemenu(){
+    navbar.style.right = '-200px';
 }
 
-*/
 
-// let menu = document.querySelector('.menu-icon');
-// let leftnav = document.querySelector('.left-nav');
+// -------------------------------learn-more-container-------------------//
+const learnMoreBtn = document.querySelector(".learnMore");
+const dropMore = document.querySelector(".learn-more-container");
+const backBtn = document.querySelector(".back-btn");
 
-// menu.onclick = function() {
-//     leftnav.classList.toggle('resize-left');
-// }
+learnMoreBtn.addEventListener('click', () => {
+    dropMore.classList.toggle('dropMoreContainer')
+
+    backBtn.addEventListener('click', () => {
+        dropMore.classList.remove('dropMoreContainer')
+    })
+})
+
+
+// -----------------------------------swipper----------------------------//
+const slide = document.querySelector(".slide"),
+    reviews = document.querySelectorAll(".reviews"),
+    arrowIcons = document.querySelectorAll(".arrowBox i");
+    
+
+
+
+// ----------------------dark-mode-section----------------//
+var darkMode = document.getElementById('darkMode');
+
+darkMode.onclick = function () {
+    document.body.classList.toggle("dark-theme");
+    if (document.body.classList.contains("dark-theme")) {
+        darkMode.style.color = 'white';
+    }
+    else {
+        darkMode.style.color = '#a78209';
+    }
+}
